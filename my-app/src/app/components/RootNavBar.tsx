e77c4bda0e23111923c2a791f411f1c9931b0c7b
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-07-30 16:34:31 +0100

    chore: basic home page ui

---
+++ my-app/src/app/components/RootNavBar.tsx
@@ -0,0 +1,16 @@
+const RootNavBar = () => {
+  return (
+    <div
+      style={{
+        backgroundColor: "red",
+        display: "flex",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <h1>Nav Bar</h1>
+      <h1>lol</h1>
+    </div>
+  );
+};
+
+export default RootNavBar;
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,14 +1,21 @@
 "use client";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
+import styles from "./styles.module.css";
 
 export default function Home() {
   const { signOut } = useAuthenticator();
   return (
     <Authenticator>
-      <div>
-        Hello there
-        <button onClick={() => signOut()}>sign out</button>
+      <div className={styles.rootdiv}>
+        <h1 className={styles.header}>Welcome!</h1>
+        <h2 className={styles.subheader}>
+          Press <strong>Enter</strong> to begin using the app
+        </h2>
+        <button className={styles.enter}>Enter 😀 </button>
+        <button className={styles.logout} onClick={() => signOut()}>
+          sign out
+        </button>
       </div>
     </Authenticator>
   );
