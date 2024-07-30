580bc4cf68502c8b73d6d1c6369907e6bbea09c4
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-07-30 14:07:01 +0100

    chore: configuring amplify with frontend

---
+++ my-app/src/app/components/ConfigureAmplify.tsx
@@ -0,0 +1,17 @@
+// components/ConfigureAmplify.tsx
+"use client";
+
+import { Amplify } from "aws-amplify";
+import outputs from "../../../../amplify_outputs.json";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { ReactNode } from "react";
+
+Amplify.configure(outputs, { ssr: true });
+
+export const AuthenticationProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  return <Authenticator.Provider>{children}</Authenticator.Provider>;
+};
