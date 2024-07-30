764964edeb740d50f5c7091eb40b45b1bd041063
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-07-30 13:05:23 +0100

    chore: adding frontend files

--- amplify/microservices/resource.ts
+++ amplify/microservices/resource.ts
@@ -1,11 +1,13 @@
 import { defineFunction } from "@aws-amplify/backend";
 import { AmplifyBackendConstruct } from "../AmplifyBackend";
 import { BackendType } from "../backend";
-import { LambdaRestApi, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export const uploadToS3Function = defineFunction({
   entry: "./handler/index.ts",
 });
+
+// Make api private and only accessible if user is logged in to aws
 
 export class LambdaRestApiIntegration extends AmplifyBackendConstruct {
   constructor(backend: BackendType, id: string) {
