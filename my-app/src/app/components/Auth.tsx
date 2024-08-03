e2f26af359d0889a7a4c07373428bf0ba8c09e8c
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-08-03 09:28:37 +0100

    fix: updated dependencies

--- my-app/src/app/components/Auth.tsx
+++ my-app/src/app/components/Auth.tsx
@@ -1,12 +1,11 @@
 // components/ConfigureAmplify.tsx
 "use client";
-
-import { Amplify } from "aws-amplify";
-import outputs from "../../../../amplify_outputs.json";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 import { ReactNode } from "react";
+import config from "$AmplifyOutputs";
 
-Amplify.configure(outputs, { ssr: true });
+Amplify.configure(config, { ssr: true });
 
 export const AuthenticationProvider = ({
   children,
