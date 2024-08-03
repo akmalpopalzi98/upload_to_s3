ccce7efe46c050797a5bd82c24c3f70413b40787
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-08-03 09:38:11 +0100

    chore: minor updates

--- my-app/src/app/spaces-home/page.tsx
+++ my-app/src/app/spaces-home/page.tsx
@@ -1,4 +1,8 @@
-const SpacesPage = () => {
+import { AuthGetCredentials } from "../utils";
+
+const SpacesPage = async () => {
+  const creds = AuthGetCredentials();
+  console.log(creds);
   return (
     <div>
       <h2 style={{ textAlign: "center", padding: "10px" }}>
--- my-app/src/app/utils.ts
+++ my-app/src/app/utils.ts
@@ -1,4 +1,41 @@
-import { type S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// utils/amplify-utils.ts
+import { cookies } from "next/headers";
+import { createServerRunner } from "@aws-amplify/adapter-nextjs";
+import { fetchAuthSession } from "aws-amplify/auth/server";
+import config from "$AmplifyOutputs";
+import { CognitoIdentity } from "@aws-sdk/client-cognito-identity";
+import assert from "node:assert";
 
-export const getSignedUrls = async (client: S3Client, key: string) => {};
+interface AWSCredentials {
+  accessKeyId: string;
+  secretAccessKey: string;
+  sessionToken?: string;
+  expiration?: Date;
+}
+
+export const { runWithAmplifyServerContext } = createServerRunner({
+  config,
+});
+
+const cognito = new CognitoIdentity({ region: config.auth.aws_region });
+
+export async function AuthGetCredentials() {
+  try {
+    const authSession = await runWithAmplifyServerContext({
+      nextServerContext: { cookies },
+      operation: (contextSpec) => fetchAuthSession(contextSpec),
+    });
+    assert(authSession.tokens?.idToken, "No token found");
+    const credentials = await cognito.getCredentialsForIdentity({
+      IdentityId: authSession.identityId,
+      Logins: {
+        [`cognito-idp.${config.auth.aws_region}.amazonaws.com/${config.auth.user_pool_id}`]:
+          authSession.tokens.idToken?.toString(),
+      },
+    });
+
+    console.log(credentials.Credentials);
+  } catch (error) {
+    console.error(error);
+  }
+}
