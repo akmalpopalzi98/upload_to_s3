c54708477c9999d1ca9f46f3a131e43f4f2c2b7f
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-08-05 12:47:08 +0100

    chore: minor change

--- my-app/src/app/spaces-home/page.tsx
+++ my-app/src/app/spaces-home/page.tsx
@@ -1,8 +1,6 @@
 import { AuthGetCredentials } from "../utils";
 
 const SpacesPage = async () => {
-  const creds = AuthGetCredentials();
-  console.log(creds);
   return (
     <div>
       <h2 style={{ textAlign: "center", padding: "10px" }}>
