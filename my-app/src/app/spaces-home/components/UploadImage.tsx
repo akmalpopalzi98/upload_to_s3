a977e97cc441acefd27a5658fe6d3557c6f23067
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-08-09 11:37:16 +0100

    feat: preview image feature

--- my-app/src/app/spaces-home/components/UploadImage.tsx
+++ my-app/src/app/spaces-home/components/UploadImage.tsx
@@ -1,27 +1,52 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import styles from "../styles.module.css";
 import assert from "assert";
 
+interface FileData {
+  fileName: string;
+  fileType: string;
+  fileContent: string;
+}
+
 const UploadImage = () => {
-  const [file, setFile] = useState({});
-  const [fileUploadData, setFileUploadData] = useState({});
+  const [fileContent, setFileContent] = useState<string | undefined>();
+  const [fileUploadData, setFileUploadData] = useState<FileData | undefined>(
+    undefined
+  );
+
+  const PreviewImage = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "20px",
+      }}
+    >
+      <h4 style={{ textAlign: "center" }}>Image Preview</h4>
+      <img src={fileContent} alt="image-preview" width="400px" height="250px" />
+      <button>Upload</button>
+    </div>
+  );
+
+  const handleLoadImage = (e: ChangeEvent<HTMLInputElement>) => {
+    let files = e.target.files;
+    const reader = new FileReader();
+    assert(files, "No files found");
+
+    reader.onload = () => {
+      setFileContent(reader.result as string);
+    };
+    reader.readAsDataURL(files[0]);
+    console.log(files);
+  };
+
   return (
     <div className={styles.uploadimagediv}>
       <h3 style={{ textAlign: "center" }}>Choose File</h3>
-      <input
-        type="file"
-        onChange={(e) => {
-          let files = e.target.files;
-          assert(files, "No files found");
-          setFile({
-            fileName: files[0].name,
-            fileType: files[0].type,
-            fileData: "",
-          });
-          console.log(files[0].type);
-        }}
-      />
+      <input type="file" accept="image/*" onChange={handleLoadImage} />
+      {fileContent && PreviewImage}
     </div>
   );
 };
