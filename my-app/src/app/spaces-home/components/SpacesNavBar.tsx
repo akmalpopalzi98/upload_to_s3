e174575174b506c23f37dfa03710cc012829043a
akmalpopalzi98 <105284469+akmalpopalzi98@users.noreply.github.com>
2024-08-31 19:25:42 +0100

    chore: adding new navbar WIP

--- my-app/src/app/spaces-home/components/SpacesNavBar.tsx
+++ my-app/src/app/spaces-home/components/SpacesNavBar.tsx
@@ -4,43 +4,47 @@ import Link from "next/link";
 import styles from "../styles.module.css";
 import { usePathname } from "next/navigation";
 import { FaHome } from "react-icons/fa";
+import { AppShell } from "@mantine/core";
 
 const SpacesNavBar = () => {
   const pathname = usePathname();
   return (
-    <div
-      style={{
-        backgroundColor: "rgb(36, 63, 92)",
-        display: "flex",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        width: "100%",
-        color: "white",
-        height: "5%",
-      }}
-    >
-      <Link href="/" className={styles.navbarlink}>
-        <FaHome />
-      </Link>
-      <Link
-        className={
-          pathname === "/spaces-home" ? styles.activelink : styles.navbarlink
-        }
-        href="/spaces-home"
-      >
-        My Spaces
-      </Link>
-      <Link
-        className={
-          pathname === "/spaces-home/create-space"
-            ? styles.activelink
-            : styles.navbarlink
-        }
-        href="/spaces-home/create-space"
-      >
-        Create Space
-      </Link>
-    </div>
+    // <div
+    //   style={{
+    //     backgroundColor: "rgb(36, 63, 92)",
+    //     display: "flex",
+    //     justifyContent: "space-evenly",
+    //     alignItems: "center",
+    //     width: "100%",
+    //     color: "white",
+    //     height: "5%",
+    //   }}
+    // >
+    //   <Link href="/" className={styles.navbarlink}>
+    //     <FaHome />
+    //   </Link>
+    //   <Link
+    //     className={
+    //       pathname === "/spaces-home" ? styles.activelink : styles.navbarlink
+    //     }
+    //     href="/spaces-home"
+    //   >
+    //     My Spaces
+    //   </Link>
+    //   <Link
+    //     className={
+    //       pathname === "/spaces-home/create-space"
+    //         ? styles.activelink
+    //         : styles.navbarlink
+    //     }
+    //     href="/spaces-home/create-space"
+    //   >
+    //     Create Space
+    //   </Link>
+    // </div>
+    <AppShell navbar={{ width: "150px", breakpoint: 0 }}>
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+    </AppShell>
   );
 };
 
